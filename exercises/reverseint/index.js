29a43b30e1a4fca36f20603703bd310af36d3a9c
sabdi21 <sabdi21@hotmail.com>
2020-02-10 16:28:33 -0800

    reverse integer refactored

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,14 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let revN = parseInt(n.toString()
+    .split('')
+    .reverse()
+    .join('')
+    ) * Math.sign(n)
+    return revN
+
+}
 
 module.exports = reverseInt;
