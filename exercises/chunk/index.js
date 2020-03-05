39f05e87b6a5f20cf0f0087037065017ebd68d95
sabdi21 <sabdi21@hotmail.com>
2020-03-05 14:48:52 -0800

    chuncked done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunked = [];
+    let index = 0
+
+    while(index < array.length) {
+        chunked.push(array.slice(index, index + size));
+        index += size;
+    }
+    return chunked
+}
+
+// let chunked = []
+// 
+// for (let element of array) {
+//     let last = chunked[chunked.length - 1];
+//     console.log('last= ', last)
+
+//     if(!last || last.length === size) {
+//     chunked.push([element])
+//     } else {
+//         last.push(element)
+//     }
+// }
+// return chunked
 
 module.exports = chunk;
